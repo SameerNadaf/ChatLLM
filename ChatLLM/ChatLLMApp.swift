b096a8c56d2b5e0e6c5f920ca3e1ee24f90e9dc2
asha <asha.k.r@cachatto.com>
2025-12-02 10:20:11 +0530

    Initial Commit

---
+++ ChatLLM/ChatLLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatLLMApp.swift
+//  ChatLLM
+//
+//  Created by CIPL User01 on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatLLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
